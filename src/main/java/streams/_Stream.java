4fb423aa66982593c0ec196d63097108ec68b30c
jimhome_mac <jsr1611@outlook.com>
2022-04-29 01:03:27 +0900

    Streams API basic functions usage

---
+++ src/main/java/streams/_Stream.java
@@ -0,0 +1,60 @@
+package streams;
+
+import imperative.Main;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.IntConsumer;
+import java.util.function.ToIntFunction;
+import java.util.stream.Collectors;
+
+public class _Stream {
+    public static void main(String[] args) {
+        List<Person> people = List.of(
+                new Person("John", Gender.MALE),
+                new Person("Ali", Gender.MALE),
+                new Person("Aisha", Gender.FEMALE),
+                new Person("Maria", Gender.FEMALE),
+                new Person("Alex", Gender.MALE),
+                new Person("Alice", Gender.FEMALE),
+                new Person("Bob", Gender.PREFER_NOT_TO_SAY)
+        );
+
+//        people.stream()
+//                .map(person -> person.gender)
+//                .collect(Collectors.toSet())
+//                .forEach(System.out::println);
+
+        Function<Person, String> personStringFunction = person -> person.name;
+        ToIntFunction<String> length = String::length;
+        IntConsumer println = System.out::println; // = x -> System.out.println(x);
+
+        people.stream()
+                .map(personStringFunction)
+                .mapToInt(length)
+                .forEach(println);
+
+    }
+
+    static class Person{
+        private final String name;
+        private final Gender gender;
+
+        Person(String name, Gender gender){
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+    enum Gender{
+        MALE, FEMALE, PREFER_NOT_TO_SAY
+    }
+}
