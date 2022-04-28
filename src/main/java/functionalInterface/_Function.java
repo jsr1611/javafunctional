1a4deea0189d8c5462b53154ae587bb0b753792c
Jumanazar <jsr1611@outlook.com>
2022-04-28 12:13:42 +0900

    Function interface usage

--- src/main/java/functionalInterface/_Function.java
+++ src/main/java/functionalInterface/_Function.java
@@ -1,10 +1,30 @@
 package functionalInterface;
 
+import java.util.function.Function;
+
 public class _Function {
     public static void main(String[] args){
         int increment = increment(0);
         System.out.println(increment);
+
+        int increment2 = incrementByOneFunction.apply(1);
+        System.out.println(increment2);
+
+        int multiply = multiplyBy10Function.apply(increment2);
+        System.out.println(multiply);
+
+
+        Function<Integer, Integer> addBy1AndThenMultiplyBy10 = incrementByOneFunction.andThen(multiplyBy10Function);
+        System.out.println(addBy1AndThenMultiplyBy10.apply(1));
     }
+
+
+    static Function<Integer, Integer> incrementByOneFunction =
+            number -> number++;
+
+    static Function<Integer, Integer> multiplyBy10Function = number -> number * 10;
+
+
     static int increment(int number){
         return number + 1;
     }
