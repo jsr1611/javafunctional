4692007b6cf6020d46d899b09c1ca188702f01a7
Jumanazar <jsr1611@outlook.com>
2022-04-28 12:30:38 +0900

    Consumer Functional interface usage

---
+++ src/main/java/functionalInterface/_Consumer.java
@@ -0,0 +1,39 @@
+package functionalInterface;
+
+import java.util.function.Consumer;
+
+public class _Consumer {
+    public static void main(String[] args) {
+
+        //Normal java function
+        Customer maria = new Customer("Maria", "999999");
+        greetCustomer(maria);
+
+
+
+        // Consumer Functional interface
+        greetCustomerConsumer.accept(maria);
+
+    }
+
+    static Consumer<Customer> greetCustomerConsumer = customer ->
+            System.out.println("Hello " + customer.customerName + ", thanks for registering phone number " + customer.customerPhoneNumber);
+
+
+
+
+    static void greetCustomer(Customer customer){
+        System.out.println("Hello " + customer.customerName + ", thanks for registering phone number " + customer.customerPhoneNumber);
+    }
+
+    static class Customer{
+        private final String customerName;
+        private final String customerPhoneNumber;
+
+        public Customer(String customerName, String customerPhoneNumber) {
+            this.customerName = customerName;
+            this.customerPhoneNumber = customerPhoneNumber;
+        }
+    }
+
+}
