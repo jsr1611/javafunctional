9d85db47426337c5c0bf15310710dd4736213ced
Jumanazar <jsr1611@outlook.com>
2022-04-28 12:36:59 +0900

    BiConsumer Functional interface usage

--- src/main/java/functionalInterface/_Consumer.java
+++ src/main/java/functionalInterface/_Consumer.java
@@ -1,20 +1,29 @@
 package functionalInterface;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class _Consumer {
     public static void main(String[] args) {
 
-        //Normal java function
+        System.out.println("//Normal java function");
         Customer maria = new Customer("Maria", "999999");
         greetCustomer(maria);
 
+        greetCustomerV2(maria, true);
 
 
-        // Consumer Functional interface
+        System.out.println("// Consumer Functional interface");
         greetCustomerConsumer.accept(maria);
 
+        System.out.println("//BiConsumer Functional interface");
+        greetCustomerConsumerV2.accept(maria, true);
+
     }
+
+    static BiConsumer<Customer, Boolean> greetCustomerConsumerV2 = ((customer, showPhoneNumber) ->
+            System.out.println("Hello " + customer.customerName + ", thanks for registering phone number "
+                    + (showPhoneNumber ? customer.customerPhoneNumber : "********")));
 
     static Consumer<Customer> greetCustomerConsumer = customer ->
             System.out.println("Hello " + customer.customerName + ", thanks for registering phone number " + customer.customerPhoneNumber);
@@ -25,6 +34,13 @@ public class _Consumer {
     static void greetCustomer(Customer customer){
         System.out.println("Hello " + customer.customerName + ", thanks for registering phone number " + customer.customerPhoneNumber);
     }
+
+
+    static void greetCustomerV2(Customer customer, boolean showPhoneNumber){
+        System.out.println("Hello " + customer.customerName + ", thanks for registering phone number "
+                + (showPhoneNumber ? customer.customerPhoneNumber : "********"));
+    }
+
 
     static class Customer{
         private final String customerName;
