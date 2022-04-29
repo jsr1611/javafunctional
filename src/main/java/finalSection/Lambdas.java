63f1a8d5740e3dbfaf3261274baa1d27789add2e
Jumanazar <jsr1611@outlook.com>
2022-04-29 18:30:10 +0900

    pure functional programming rules

--- src/main/java/finalSection/Lambdas.java
+++ src/main/java/finalSection/Lambdas.java
@@ -1,6 +1,7 @@
 package finalSection;
 
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class Lambdas {
@@ -22,9 +23,23 @@ public class Lambdas {
         String upperCasedName = upperCaseName.apply("Alex", 20);
         System.out.println(upperCasedName);
 
-
-
-
-
     }
+
+    // properties of functional programming
+    /*
+
+    Pure functional programming has a set of rules to follow:
+
+    - No state
+
+    - Pure Functions
+
+    - No Side Effects
+
+    - Higher Order Functions
+
+    * The function takes one or more functions as parameters.
+    * The function returns another function as a result.
+     */
+
 }
